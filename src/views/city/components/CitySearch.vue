9337b288ce161e86c1324188819a4b500c5fd9ef
chenzhiwei0109 <1138476755@qq.com>
2020-04-25 21:47:49 +0800

    搜索框

---
+++ src/views/city/components/CitySearch.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" placeholder="请输入你想去的城市" />
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+<style lang="scss" scoped>
+  @import "~styles/config.scss";
+  .search {
+    @include bgColor(50deg);
+    height: 0.72rem;
+    padding: 0 0.1rem;
+    &-input {
+      box-sizing: border-box;
+      padding: 0 0.1rem;
+      width: 100%;
+      height: 0.62rem;
+      line-height: 0.62rem;
+      text-align: center;
+      border-radius: 6px;
+      color: #616161;
+    }
+  }
+</style>
+
